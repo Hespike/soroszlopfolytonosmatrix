31b3dde33336a9c299d282474dfb359654568f90
Hespi <sardi.andras@gmail.com>
2020-10-18 22:00:57 +0200

    Create soroszlopfolytonosmatrix.c

---
+++ soroszlopfolytonosmatrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define N 3
+
+int main() {
+    int tomb[N][N];
+    int i, j;
+    for (i = 0; i < N; ++i) {           //  a különböző ciklusokat egybásba is lehet ágyazni
+        for (j = 0; j < N; ++j) {       // jelen esetben N-szer fog lefutni a külső for ciklus
+            scanf("%d", &(tomb[i][j])); // és (N*N)-szer a belső
+        }
+    }
+    for (i = 0; i < N; ++i) {            // természetesen itt is és a következőnél is ugyanaz a helyzet
+        for (j = 0; j < N; ++j) {
+            printf("%d ", tomb[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (j = 0; j < N; ++j) {
+        for (i = 0; i < N; ++i) {
+            printf("%d ", tomb[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}ltsd fel elemekkel, majd írasd ki az elemeit sor- illetve oszlopfolytonosan is
